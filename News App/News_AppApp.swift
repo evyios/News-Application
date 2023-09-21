fa44364a3b3ed282764fdebb9f9cf1b554b1f27e
Evgeny Yachny <ev.yachny@gmail.com>
2023-09-21 11:37:24 +0300

    Initial Commit

---
+++ News App/News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_AppApp.swift
+//  News App
+//
+//  Created by Evgeny on 21.09.23.
+//
+
+import SwiftUI
+
+@main
+struct News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
