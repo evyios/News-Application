6c3241f5e1cd3bfbf35b75e7198bea3e8a66ee01
Evgeny Yachny <ev.yachny@gmail.com>
2023-10-02 19:44:12 +0300

    start building detail view

---
+++ News App/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  News App
+//
+//  Created by Evgeny on 30.09.23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
