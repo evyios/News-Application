a5a18a3fe5fd4fa180a5a8603c9945c8edfc40b9
Evgeny Yachny <ev.yachny@gmail.com>
2023-10-02 21:09:35 +0300

    Detail view image + scroll view

--- News App/Views/DetailView.swift
+++ News App/Views/DetailView.swift
@@ -6,13 +6,46 @@
 //
 
 import SwiftUI
+import CachedAsyncImage
 
 struct DetailView: View {
+    
+    let news: News
+    
     var body: some View {
-        Text("Detail View")
+        VStack {
+            VStack {
+                CachedAsyncImage(url: URL(string: news.imageUrl), transaction: Transaction(animation: .easeInOut)) { item in
+                    if let image = item.image {
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                    } else {
+                        HStack {
+                            ProgressView()
+                        }
+                    }
+                }
+                .ignoresSafeArea()
+            }
+            .frame(height: UIScreen.main.bounds.height / 2.7)
+            
+            ScrollView(.vertical) {
+                    
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background {
+                Color(.background)
+                    .clipShape(CustomCorner(corners: [.topLeft, .topRight], radius: 25))
+                    .ignoresSafeArea()
+
+            }
+            .offset(y: -40)
+        }
+        .background(Color(.background))
     }
 }
 
 #Preview {
-    DetailView()
+    DetailView(news: DeveloperPreview.instance.news)
 }
